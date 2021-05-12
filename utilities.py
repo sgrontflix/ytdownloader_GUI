9462034b1a7ecd202a50ebc8f729476458e40201
sgrontflix <75019076+sgrontflix@users.noreply.github.com>
2021-05-12 22:59:48 +0200

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,6 +1,9 @@
 import os
 import re
 import subprocess
+from shlex import split
+from collections import namedtuple
+from functools import reduce
 
 
 def youtube_url_validation(url):
@@ -54,9 +57,46 @@ def remove_files(files):
     return files
 
 
+"""
+The following three functions are needed to avoid using check_output() with shell=True
+
+shell=True should be avoided at all costs as explained here:
+https://stackoverflow.com/questions/3172470/actual-meaning-of-shell-true-in-subprocess
+
+Functions are from here:
+https://stackoverflow.com/questions/24306205/file-not-found-error-when-launching-a-subprocess-containing-piped-commands
+"""
+proc_output = namedtuple('proc_output', 'stdout stderr')
+
+
+def pipeline(starter_command, *commands):
+    if not commands:
+        try:
+            starter_command, *commands = starter_command.split('|')
+        except AttributeError:
+            pass
+    starter_command = _parse(starter_command)
+    starter = subprocess.Popen(starter_command, stdout=subprocess.PIPE)
+    last_proc = reduce(_create_pipe, map(_parse, commands), starter)
+    return proc_output(*last_proc.communicate())
+
+
+def _create_pipe(previous, command):
+    proc = subprocess.Popen(command, stdin=previous.stdout, stdout=subprocess.PIPE)
+    previous.stdout.close()
+    return proc
+
+
+def _parse(cmd):
+    try:
+        return split(cmd)
+    except Exception:
+        return cmd
+
+
 def is_gpu_available():
     try:
-        subprocess.check_output('nvidia-smi', shell=True)
+        pipeline('nvidia-smi')
         return True
     except Exception:
         return False
